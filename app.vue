3e2906928f7223a2b32c4a3dffd8e23bbcf389da
Deji Kuye <dkuyebox@gmail.com>
2024-06-09 19:02:32 +0100

    setup

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <h1>{{ config.public.apiUrl }}</h1>
+
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig()
+
+console.log('config.public.apiUrl', config.public.apiUrl)
+</script>
+
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,52 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'eClass - Learning Platform',
+      meta: [
+        {
+          name: 'description',
+          content:
+            'eClass is a dynamic online learning hub tailored for the Nigerian student. Our platform is the brainchild of educators, tech enthusiasts, and professionals who recognized the need for a comprehensive and accessible educational resource. What started as an idea has evolved into a thriving community dedicated to shaping the future of education.'
+        },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'og:title', content: 'eClass - Learning Platform' },
+        {
+          name: 'og:description',
+          content:
+            'eClass is a dynamic online learning hub tailored for the Nigerian student. Our platform is the brainchild of educators, tech enthusiasts, and professionals who recognized the need for a comprehensive and accessible educational resource. What started as an idea has evolved into a thriving community dedicated to shaping the future of education.'
+        },
+        { name: 'og:image', content: 'https://cdn.eclass.ng/logo/eclass-square.png' },
+        { name: 'og:url', content: 'https://eclass.ng' },
+        { name: 'og:type', content: 'website' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        // Twitter
+        { name: 'twitter:title', content: 'eClass - Learning Platform' },
+        {
+          name: 'twitter:description',
+          content:
+            'eClass is a dynamic online learning hub tailored for the Nigerian student. Our platform is the brainchild of educators, tech enthusiasts, and professionals who recognized the need for a comprehensive and accessible educational resource. What started as an idea has evolved into a thriving community dedicated to shaping the future of education.'
+        },
+        { name: 'twitter:image', content: 'https://cdn.eclass.ng/logo/eclass-square.png' }
+      ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.svg'
+        }
+      ]
+    }
+  },
+
+  //
+  runtimeConfig: {
+    // Public
+    public: {
+      apiUrl: '',
+      apiKey: ''
+    }
+  },
+
   devtools: { enabled: true }
 })
